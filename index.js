9aea03e46e01f5511895c5b347c17166e7eb49a8
Clara <c_c_456@hotmail.com>
2017-12-11 17:20:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,4 +23,6 @@
 // }
 
 
-BoardMember.prototype.
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+};
